06d04f7bd8b170cc65b9c228f58a65904d153bde
Jonathan Jasper <jfj2121@columbia.edu>
2020-10-15 03:54:44 +0000

    Creates Export Function

--- map/views.py
+++ map/views.py
@@ -5,4 +5,5 @@ def default_map(request):
     sightings = Squirrel.objects.all()
     context = {'sightings': sightings}
     return render(request, 'map/map.html', context)
+        
 
--- sightings/management/commands/export_squirrel_data.py
+++ sightings/management/commands/export_squirrel_data.py
@@ -1,5 +1,20 @@
 from django.core.management.base import BaseCommand, CommandError
 import csv 
 from sightings.models import Squirrel 
+import sys 
 
+class Command(BaseCommand):
 
+    def add_arguments(self, parser):
+        parser.add_argument('path',type=str,help="csv file")
+
+    def handle(self, path, **options):
+        with open(path, 'w', newline='') as field1:
+            model = Squirrel
+            field_names = [field2.name for field2 in model._meta.fields]
+            writer  = csv.writer(field1, quoting=csv.QUOTE_ALL)
+            writer.writerow(field_names)
+            for instance in model.objects.all():
+                writer.writerow([getattr(instance, field3) for field3 in field_names])
+
+        self.stdout.write(self.style.SUCCESS('Successfully exported!')) 
