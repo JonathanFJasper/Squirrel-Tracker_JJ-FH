02cc2a2116264379c6276dc30f99607a6b23d4be
Jonathan Jasper <jfj2121@columbia.edu>
2020-10-15 22:35:44 +0000

    Updated Index Page

--- sightings/views.py
+++ sightings/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Squirrel
 
 def index(request):
-    return render (request, 'sightings/index.html',[])
-
+    Sightings=Squirrel.objects.all()
+    context={'sightings':Sightings,}
+    return render (request, 'sightings/index.html',context)
