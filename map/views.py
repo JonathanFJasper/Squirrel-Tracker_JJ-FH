9ab8fc2262002725015b71d72ce409039785cc0c
Fabio Hodo <72325950+Fabius2421@users.noreply.github.com>
2020-10-15 18:22:17 -0400

    Show only first 99 squirrel sightings

--- map/views.py
+++ map/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from sightings.models import Squirrel
 
 def index(request):
-    sightings = Squirrel.objects.all()
+    sightings = Squirrel.objects.all()[:98]
     context = {'sightings': sightings}
     return render(request, 'map/index.html', context)
         
