55918ad82a1f1ef566c3043ca4a049259aa08a9f
Jonathan Jasper <jfj2121@columbia.edu>
2020-10-16 21:33:05 +0000

    Added Update and Add Views and URLS

--- sightings/urls.py
+++ sightings/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
         path('', views.index,name="index"),
         path('<Unique_Squirrel_ID>/details/',views.details,name="details"),
+        path('add/',views.add,name="add"),
+        path('<Unique_Squirrel_ID>/',views.update,name="update"),
         ]
--- sightings/views.py
+++ sightings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Squirrel
+from .forms import SquirrelForm
 
 def index(request):
     Sightings=Squirrel.objects.all()
@@ -11,3 +12,29 @@ def details(request,Unique_Squirrel_ID):
     squirrel = Squirrel.objects.get(Unique_Squirrel_ID=Unique_Squirrel_ID)
     context = {'squirrel':squirrel,}
     return render(request,'sightings/details.html',context)
+
+def add(request):
+    if request.method == "POST":
+        form = SquirrelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            context={'sightings':Squirrel.objects.all(),}
+            return render(request,'sightings/index.html',context)
+    else:
+        form = SquirrelForm()
+    context = {'form':form}
+    return render(request, 'sightings/update.html',context)
+
+def update(request,Unique_Squirrel_ID):
+    squirrel = Squirrel.objects.get(Unique_Squirrel_ID=Unique_Squirrel_ID)
+    if request.method == "POST":
+        form = SquirrelForm(request.POST, instance=Sightings)
+        if form.is_valid():
+            form.save()
+            context={'squirrel':Squirrel.objects.all(),}
+            return render(request,'sightings/index.html',context)
+    else:
+        form = SquirrelForm(instance=Sightings)
+    context = {'form':form}
+    return render(request, 'sightings/update.html',context)
+
